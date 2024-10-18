522524ff07f75fc299c8691ae2338e18bbbe2c1d
Davin <71451687+bc-davin@users.noreply.github.com>
2024-10-18 15:59:11 -0600

    removes things

--- server/server.js
+++ server/server.js
@@ -2,16 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 
 app.get("/api/home", (req, res) =>{
     res.json({message: "Hello World!"});
-});
-
-app.get("/api/userInfo", (req, res) => {
-    res.json({user: "test@gamil.com", password: "password"});
-});
-
-app.listen(PORT, () => {
-    console.log(`Server Started on port ${PORT}`);
 });
