3a12d669cd8c48825c36e27c9582c32c2d207c14
Mathew Maki <mathewmaki2002@gmail.com>
2024-10-18 17:29:57 -0600

    New users are added to the database from the sign up page

--- client/app/data/actions/auth-actions.ts
+++ client/app/data/actions/auth-actions.ts
@@ -1,4 +1,7 @@
+// app/data/actions/auth-actions.ts
+
 'use server';
+
 
 export async function registerUserAction(prevState: any, formData: FormData) {
     console.log("Hello from Register User Action!");
@@ -9,9 +12,51 @@ export async function registerUserAction(prevState: any, formData: FormData) {
         name:   formData.get("name"),
         institution: formData.get("institution"),
       };
-    
-      return {
-        ...prevState,
-        data: fields,
+
+      try {
+        const response = await fetch('http://localhost:5001/api/users/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                user_mail: fields.email,
+                user_password_md5: fields.password,  // TODO Use SHA 256 hashing
+                user_name: fields.name,
+                user_school: fields.institution,
+                is_enabled: true,  // Default values for new users
+                is_removed: false, // Default values for new users
+            }),
+
+        });
+        if (!response.ok) {
+            throw new Error('Failed to register user');
+        }
+
+        const data = await response.json();
+
+        return {
+            ...prevState,
+            data,
+        };
+      } catch (error) {
+        let errorMessage = 'An unknown error occurred';
+        
+        if (error instanceof Error) {
+            // TypeScript now knows 'error' is of type 'Error'
+            errorMessage = error.message;
+        } else {
+            // For safety, in case error isn't of type 'Error'
+            errorMessage = String(error);
+        }
+
+        console.error("Error registering user:", errorMessage);
+
+        return {
+            ...prevState,
+            error: errorMessage,
+        };
       }
+
+      
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,7 +5,7 @@ const userController = require('../controller/user.controller');
 
 const router = express.Router();
 router.get('/', userController.getAllUsers);
-router.post('/', userController.createUser);
+router.post('/register', userController.createUser);
 
 
 module.exports = router;
